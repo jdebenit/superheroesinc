73d6a7575e587f48256abe07facc3d738c5b4114
Jorge Francisco de Benito Montoya <jf.benito@gmail.com>
2026-01-11 21:06:38 +0100

    add fix

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -8,25 +8,30 @@ export async function GET(context) {
     description: 'Noticias, artículos y novedades sobre juegos de rol de superhéroes.',
     site: context.site,
     items: blog
-        .sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf())
-        .map((post) => ({
-      title: post.data.title,
-      pubDate: post.data.pubDate,
-      description: post.data.description,
-      // Compute RSS link from post `slug`
-      // This example assumes all blog posts are rendered as `/blog/[slug]` routes
-      link: `/blog/${post.slug}/`,
-      author: post.data.author,
-      content: post.body, 
-      customData: `
+      .sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf())
+      .map((post) => ({
+        title: post.data.title,
+        pubDate: post.data.pubDate,
+        description: post.data.description,
+        // Compute RSS link from post `slug`
+        // This example assumes all blog posts are rendered as `/blog/[slug]` routes
+        link: `/blog/${post.slug}/`,
+        author: post.data.author,
+        content: post.body,
+        customData: `
       <media:content
           url="${post.data.image ? new URL(post.data.image, context.site) : ''}"
           medium="image"
           type="image/jpeg"
       />
       `
-    })),
-    customData: `<language>es-es</language>`,
-    stylesheet: '/rss/styles.xsl', // optional
+      })),
+  })),
+  customData: `<language>es-es</language>`,
+    xmlns: {
+    media: 'http://search.yahoo.com/mrss/',
+      atom: 'http://www.w3.org/2005/Atom', // Optional but good practice
+    },
+  stylesheet: '/rss/styles.xsl', // optional
   });
 }
