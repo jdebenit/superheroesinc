fc69c959c779b00421ee06e7ab290ab2a5d9ebba
Jorge Francisco de Benito Montoya <jf.benito@gmail.com>
2026-01-27 14:34:51 +0100

    fix

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,5 +1,8 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
+import MarkdownIt from 'markdown-it';
+
+const parser = new MarkdownIt();
 
 export async function GET(context) {
   const blog = await getCollection('blog');
@@ -10,8 +13,11 @@ export async function GET(context) {
     xmlns: {
       media: 'http://search.yahoo.com/mrss/',
       atom: 'http://www.w3.org/2005/Atom',
+      dc: 'http://purl.org/dc/elements/1.1/',
     },
-    customData: `<language>es-es</language>`,
+    customData: `<language>es-es</language>
+<atom:link href="${context.site}rss.xml" rel="self" type="application/rss+xml" />
+<copyright>Copyright ${new Date().getFullYear()}, Superheroes Inc.</copyright>`,
     stylesheet: '/rss/styles.xsl',
     items: blog
       .sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf())
@@ -20,9 +26,11 @@ export async function GET(context) {
         pubDate: post.data.pubDate,
         description: post.data.description,
         link: `/blog/${post.slug}/`,
-        author: post.data.author,
-        content: post.body,
-        customData: post.data.image ? `<media:content url="${new URL(post.data.image, context.site).toString()}" medium="image" type="image/jpeg" />` : '',
+        author: post.data.author, // Keep standard author for backward compatibility if needed, though often expects email
+        categories: post.data.tags,
+        content: parser.render(post.body),
+        customData: (post.data.image ? `<media:content url="${new URL(post.data.image, context.site).toString()}" medium="image" type="image/jpeg" />` : '') +
+          `<dc:creator>${post.data.author}</dc:creator>`,
       })),
   });
 }
