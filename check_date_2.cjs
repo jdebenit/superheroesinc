7b7d23026489af5f0e3726bb970035a0e530b486
Jorge Francisco de Benito Montoya <jf.benito@gmail.com>
2026-03-01 16:46:50 +0100

    remove trash

--- check_date_2.cjs
+++
@@ -1,3 +0,0 @@
-
-console.log('Date("0650-01-01") year:', new Date("0650-01-01").getFullYear());
-console.log('Date("0500-01-01") year:', new Date("0500-01-01").getFullYear());
--- test-parada.js
+++
@@ -1,4 +0,0 @@
-// Debug test - checking what AGI 90 produces
-const agi = 90;
-const paradaFisica = Math.floor(agi / 4);
-console.log('TEST: AGI 90 -> Physical Parry should be:', paradaFisica); // Should be 22
